ada540512cdf66756976c30672be252d4a1a95aa
pratik9815 <pchamlagain12@gmail.com>
2025-01-11 21:16:58 +0545

    initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.Application.DTOs;
+using UserManagement.Infrastructure.IRepositories;
+using LoginRequest = UserManagement.Application.DTOs.LoginRequest;
+
+namespace UserManagement.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        public IActionResult Login()
+        {
+            return View(new LoginRequest());
+        }
+        [HttpPost]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(request);
+                }
+                var response = await _authService.LoginAsync(request);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IActionResult Register()
+        {
+            return View(new CreateApplicationUserDTO());
+        }   
+
+        [HttpPost]
+        public async Task<ActionResult<AuthResponse>> Register(CreateApplicationUserDTO request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(request);
+                }
+                var response = await _authService.RegisterAsync(request);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
